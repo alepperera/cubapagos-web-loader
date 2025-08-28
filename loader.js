5572b32958568fabeb12989b5f65d0d84966966a
alepperera <80180869+alepperera@users.noreply.github.com>
2025-08-28 12:19:05 -0400

    Update loader.js

--- loader.js
+++ loader.js
@@ -16,8 +16,8 @@ overlay.style.cssText = `
 const lottieContainer = document.createElement('div');
 lottieContainer.id = 'lottieContainer';
 lottieContainer.style.cssText = `
-  max-width: 50%;
-  max-height: 50%;
+  max-width: 50px;
+  max-height: 50px;
   display: none;
   position: absolute;
   top: 50%;
